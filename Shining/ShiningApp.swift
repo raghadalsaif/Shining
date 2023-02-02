84fb597006809d0e8cd334f4fff86bae7dfd101c
raghad <raghad.k.alsaif@gmail.com>
2023-02-02 14:54:21 +0300

    Initial Commit

---
+++ Shining/ShiningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiningApp.swift
+//  Shining
+//
+//  Created by raghad khalid alsaif on 11/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ShiningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
